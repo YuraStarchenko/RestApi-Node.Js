c10d688facba565bad117905416097f8a24025af
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-07-11 21:48:57 +0300

    add

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -33,22 +33,21 @@ const login = async (req, res) => {
   const user = await User.findOne({ email });
 
   if (!user) {
-    throw HttpError(401, `${email} or password is wrong`);
-	}
-	
-	const passwordCompare = bcrypt.compare(password, user.password);
-	
+    throw HttpError(401, `${email} or password is wrong ⚠️`);
+  }
+
+  const passwordCompare = await bcrypt.compare(password, user.password);
   if (!passwordCompare) {
-    throw HttpError(401, `${email} or password is wrong`);
+    throw HttpError(401, `${password} is wrong ⚠️`);
   }
 
   const payload = {
     id: user._id,
   };
 
-	const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
-	
-	await User.findByIdAndUpdate(user._id, { token });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+
+  await User.findByIdAndUpdate(user._id, { token });
 
   res.json({
     token,
@@ -58,8 +57,6 @@ const login = async (req, res) => {
     },
   });
 };
-
-
 
 module.exports = {
   register: ctrlWrapper(register),
