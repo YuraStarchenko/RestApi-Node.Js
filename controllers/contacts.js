eca39f5064b30095e18b27507ba9cc9cacc13324
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-07-15 21:52:24 +0300

    add

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,7 +4,12 @@ const Contact = require("../models/contacts");
 
 const listContacts = async (req, res) => {
 	const { _id: owner } = req.user;
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt");
+	const { page = 1, limit = 20 } = req.query;
+	const skip = (page - 1) * limit;
+  const result = await Contact.find(
+    { owner },
+    "-createdAt -updatedAt", {skip, limit}
+  ).populate("owner", "email subscription");
   res.json(result);
 };
 
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -46,9 +46,7 @@ const login = async (req, res) => {
   };
 
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
-
   await User.findByIdAndUpdate(user._id, { token });
-
   res.json({
     token,
     user: {
@@ -58,7 +56,27 @@ const login = async (req, res) => {
   });
 };
 
+const getCurrent = async (req, res) => {
+  const { email, subscription } = req.user;
+
+  res.json({
+    email,
+    subscription,
+  });
+};
+
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
+
+  res.json({
+    message: "No Content",
+  });
+};
+
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
+  getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
 };
