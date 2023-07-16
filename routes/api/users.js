a94ff233d7db79798fd44c3906c4f9a7473e17ba
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-07-16 15:24:16 +0300

    add

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,5 +9,6 @@ router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrent);
 router.post("/logout", authenticate, ctrl.logout);
+router.patch("/users/subscription",);
 
 module.exports = router;
